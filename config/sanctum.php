a5202c69e590057a8c4f04aa1bc78463c1e72073
Kristian Binau <kristian@binau.me>
2022-05-11 14:47:47 +0200

    Change config - Enable API and stateful domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,restaurant-pwa.binau.dev,binau.dev',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
