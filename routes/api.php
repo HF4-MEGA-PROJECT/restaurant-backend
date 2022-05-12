a9ad698496ce0c106a3039e52175af507b0c2619
frederikpyt <frederikpyt@gmail.com>
2022-05-12 09:46:07 +0200

    Added reservation migration, model, controller, factory, route and feature tests

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('category', \App\Http\Controllers\CategoryController::class);
+Route::apiResource('reservation', \App\Http\Controllers\ReservationController::class);
