b5048028eec5520e7b1d407c2d2a9c035cc0c6c8
frederikpyt <frederikpyt@gmail.com>
2022-05-18 08:36:01 +0200

    Removed the welcome page

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,11 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (\Illuminate\Support\Facades\Auth::check()) {
+        return redirect('/dashboard');
+    }
+
+    return redirect('/login');
 });
 
 Route::middleware([
