81c216fbf82b03ab8ba02253e14a43f8b98c6bc2
neerholt <victorneerholt@gmail.com>
2022-06-03 10:26:51 +0200

    #44 - Changes some values

--- tests/Feature/ProductChild/ControllerTest.php
+++ tests/Feature/ProductChild/ControllerTest.php
@@ -20,4 +20,40 @@ class ControllerTest extends TestCase
         $products = $category->products()->get();
         $response->assertExactJson($products->toArray());
     }
+
+
+    public function test_products_can_be_fetched_when_some_products_exist()
+    {
+        $category = Category::factory()->create();
+        Product::factory()->create(['category_id' => $category->id]);
+        Product::factory()->create(['category_id' => $category->id]);
+        Product::factory()->create(['category_id' => $category->id]);
+        Product::factory()->create();
+        Product::factory()->create();
+        Product::factory()->create();
+
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->getJson(route('category.products.index', $category));
+
+        $products = $category->products()->get();
+
+        $response->assertExactJson($products->toArray());
+    }
+
+
+    public function test_categories_can_be_fetched_when_no_sub_categories_exist()
+    {
+        $category = Category::factory()->create();
+
+        $this->actingAs($user = User::factory()->create());
+
+        $response = $this->getJson(route('category.products.index', $category));
+
+        $products = $category->products()->get();
+
+        $response->assertExactJson($products->toArray());
+    }
+
+
 }
