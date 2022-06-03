0467b4a4ebe23d0d3a3f6b0cc2b173c8043da883
neerholt <victorneerholt@gmail.com>
2022-06-03 09:29:39 +0200

    #44 - Add unit test to fetch one product

---
+++ tests/Feature/ProductChild/ControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\ProductChild;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_product_can_be_fetched_when_one_product_exist(){
+        $product = Product::factory()->create();
+        Product::factory()->create(['category_id' => $product->id]);
+
+        $this->actingAs($user = User::factory()->create());
+        $response = $this->getJson(route('product.children.index', $product));
+        $products = $product->sub_categories()->get();
+
+        $response->assertExactJson($products->toArray());
+    }
+
+}
