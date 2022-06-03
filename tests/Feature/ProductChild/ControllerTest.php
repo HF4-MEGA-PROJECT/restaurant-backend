891c00d40f2e612df1cbd9c5ea31f5dfb21dd80b
neerholt <victorneerholt@gmail.com>
2022-06-03 10:09:42 +0200

    #44 - Changes some values

--- tests/Feature/ProductChild/ControllerTest.php
+++ tests/Feature/ProductChild/ControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\ProductChild;
 
 use App\Models\Category;
-use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -13,14 +12,11 @@ class ControllerTest extends TestCase
     use RefreshDatabase;
 
     public function test_product_can_be_fetched_when_one_product_exist(){
-        $product = Product::factory()->create();
-        Product::factory()->create(['category_id' => $product->categories_id]);
-
+        $product = Category::factory()->create();
+        Category::factory()->create(['category_id' => $product->categories_id]);
         $this->actingAs($user = User::factory()->create());
         $response = $this->getJson(route('category.products.index', $product));
         $products = $product->products()->get();
-
         $response->assertExactJson($products->toArray());
     }
-
 }
