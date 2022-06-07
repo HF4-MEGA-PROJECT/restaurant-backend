41ced3b4b1aa56a5e205112c5ba709b6f8c84dc4
Kristian Binau <kristian@binau.me>
2022-06-07 08:43:27 +0200

    #94 - Added types to products as migration

---
+++ database/migrations/2022_06_07_074341_update_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\ProductTypes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->enum('type', [ProductTypes::FOOD->value, ProductTypes::DRINKS->value, ProductTypes::SNACKS->value])->default('food')->after('price_at_purchase');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->removeColumn('type');
+        });
+    }
+};
